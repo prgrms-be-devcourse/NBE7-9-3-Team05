bd80d8bd3ac36770d1a6e1809e851d0e1012efd1
gksdud1109 <vectorh532@gmail.com>
2025-11-13 17:51:00 +0900

    refactor: responseAspect 코틀린 전환

--- motionit/src/main/java/com/back/motionit/global/aspect/ResponseAspect.kt
+++ motionit/src/main/java/com/back/motionit/global/aspect/ResponseAspect.kt
@@ -1,51 +1,39 @@
-package com.back.motionit.global.aspect;
+package com.back.motionit.global.aspect
 
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.springframework.stereotype.Component;
-
-import com.back.motionit.global.respoonsedata.ResponseData;
-
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
+import com.back.motionit.global.respoonsedata.ResponseData
+import jakarta.servlet.http.HttpServletResponse
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
+import org.springframework.stereotype.Component
 
 @Aspect
 @Component
-@RequiredArgsConstructor
-public class ResponseAspect {
+class ResponseAspect(
+    private val response: HttpServletResponse
+) {
 
-	private final HttpServletResponse response;
+    @Around(
+        """
+        (
+            within(@org.springframework.web.bind.annotation.RestController *)
+            &&
+            (
+                @annotation(org.springframework.web.bind.annotation.GetMapping) ||
+                @annotation(org.springframework.web.bind.annotation.PostMapping) ||
+                @annotation(org.springframework.web.bind.annotation.PutMapping) ||
+                @annotation(org.springframework.web.bind.annotation.DeleteMapping)
+            )
+        ) ||
+        @annotation(org.springframework.web.bind.annotation.ResponseBody)
+        """
+    )
+    fun responseAspect(joinPoint: ProceedingJoinPoint): Any? {
+        val rst = joinPoint.proceed() // 실제 수행 메서드
 
-	@Around("""
-		(
-			within
-			(
-				@org.springframework.web.bind.annotation.RestController *
-			)
-			&&
-			(
-				@annotation(org.springframework.web.bind.annotation.GetMapping)
-				||
-				@annotation(org.springframework.web.bind.annotation.PostMapping)
-				||
-				@annotation(org.springframework.web.bind.annotation.PutMapping)
-				||
-				@annotation(org.springframework.web.bind.annotation.DeleteMapping)
-			)
-		)
-		||
-		@annotation(org.springframework.web.bind.annotation.ResponseBody)
-		""")
-	public Object responseAspect(ProceedingJoinPoint joinPoint) throws Throwable {
-
-		Object rst = joinPoint.proceed(); // 실제 수행 메서드
-
-		if (rst instanceof ResponseData responseData) {
-			response.setStatus(responseData.getStatusCode());
-		}
-
-		return rst;
-	}
-
+        if (rst is ResponseData<*>) {
+            response.status = rst.statusCode
+        }
+        return rst
+    }
 }
