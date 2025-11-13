0cd4298eeb05e9597a81609331733284894ce6a6
SicpamaMinwoo <minwoo@sicpama.com>
2025-11-13 11:59:15 +0900

    chore: kotlin configuration in build gradle

--- motionit/build.gradle.kts
+++ motionit/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     java
     id("org.springframework.boot") version "3.5.6"
     id("io.spring.dependency-management") version "1.1.7"
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    kotlin("plugin.jpa") version "1.9.25"
     checkstyle
     jacoco
 }
@@ -35,6 +38,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-websocket")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
 
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
