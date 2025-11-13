2e94343cc364ae3bfc45bb620126c070fc0e3fe8
Minwoo Lee <60373193+LeeMinwoo115@users.noreply.github.com>
2025-11-13 12:36:41 +0900

    chore: 코틀린 변환에 필요한 의존성 추가
    * chore: kotlin configuration in build gradle

--- motionit/build.gradle.kts
+++ motionit/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     java
     id("org.springframework.boot") version "3.5.6"
     id("io.spring.dependency-management") version "1.1.7"
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    kotlin("plugin.jpa") version "1.9.25"
     checkstyle
     jacoco
 }
@@ -27,18 +30,23 @@ repositories {
 }
 
 dependencies {
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    runtimeOnly("com.h2database:h2")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("io.github.openfeign.querydsl:querydsl-core:7.1")
+
     implementation("org.springframework.boot:spring-boot-starter-batch")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.security:spring-security-messaging")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-starter-web")
+
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-websocket")
 
     compileOnly("org.projectlombok:lombok")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
     annotationProcessor("org.projectlombok:lombok")
 
@@ -70,6 +78,12 @@ dependencies {
     // actuator, micrometer
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("io.micrometer:micrometer-registry-prometheus")
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
 }
 
 tasks.withType<JavaCompile>().configureEach {
