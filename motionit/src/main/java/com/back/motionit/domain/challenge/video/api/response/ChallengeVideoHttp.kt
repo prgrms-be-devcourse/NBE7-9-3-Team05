0d434f1bb1c0744514940e2c99858b56c2f85e59
gksdud1109 <vectorh532@gmail.com>
2025-11-14 14:08:26 +0900

    refactor: video controller api 인터페이스 코틀린 전환

--- motionit/src/main/java/com/back/motionit/domain/challenge/video/api/response/ChallengeVideoHttp.kt
+++ motionit/src/main/java/com/back/motionit/domain/challenge/video/api/response/ChallengeVideoHttp.kt
@@ -1,18 +1,15 @@
-package com.back.motionit.domain.challenge.video.api.response;
+package com.back.motionit.domain.challenge.video.api.response
 
-public class ChallengeVideoHttp {
-	// 영상 업로드
-	public static final String UPLOAD_SUCCESS_CODE = "200";
-	public static final String UPLOAD_SUCCESS_MESSAGE = "영상 업로드 성공";
+object ChallengeVideoHttp {
+    // 영상 업로드
+    const val UPLOAD_SUCCESS_CODE: String = "200"
+    const val UPLOAD_SUCCESS_MESSAGE: String = "영상 업로드 성공"
 
-	// 오늘의 미션 영상 조회
-	public static final String GET_TODAY_MISSION_SUCCESS_CODE = "200";
-	public static final String GET_TODAY_MISSION_SUCCESS_MESSAGE = "오늘의 미션 영상 조회 성공";
+    // 오늘의 미션 영상 조회
+    const val GET_TODAY_MISSION_SUCCESS_CODE: String = "200"
+    const val GET_TODAY_MISSION_SUCCESS_MESSAGE: String = "오늘의 미션 영상 조회 성공"
 
-	//  영상 삭제
-	public static final String DELETE_SUCCESS_CODE = "200";
-	public static final String DELETE_SUCCESS_MESSAGE = "영상이 삭제되었습니다.";
-
-	private ChallengeVideoHttp() {
-	}
+    //  영상 삭제
+    const val DELETE_SUCCESS_CODE: String = "200"
+    const val DELETE_SUCCESS_MESSAGE: String = "영상이 삭제되었습니다."
 }
