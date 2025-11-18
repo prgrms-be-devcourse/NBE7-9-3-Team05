269d53ec8c5b86bed6a8e1a58adcaa1fd15be3c8
SicpamaMinwoo <minwoo@sicpama.com>
2025-11-18 11:15:12 +0900

    refactor: kapt 의존성 추가

--- motionit/build.gradle.kts
+++ motionit/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
     implementation("io.github.openfeign.querydsl:querydsl-core:7.1")
+    kapt("io.github.openfeign.querydsl:querydsl-apt:7.1:jpa")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     implementation("org.springframework.boot:spring-boot-starter-batch")
