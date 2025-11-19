b634f948f7ed2353263038ae9f86e9cfaa38fbb6
SicpamaMinwoo <minwoo@sicpama.com>
2025-11-19 14:52:01 +0900

    refactor: Jacoco 커버리지 필요없는 파일들 커버리지 계산에서 제거

--- motionit/build.gradle.kts
+++ motionit/build.gradle.kts
@@ -132,6 +132,7 @@ val coverageExcludes = listOf(
     "**/api/**",
     "**/constants/**",
     "**/error/**",
+    "**/init/**",
     "**/exception/**",
     "**/vo/**",
     "**/Q*.*",                 // Querydsl 생성물
